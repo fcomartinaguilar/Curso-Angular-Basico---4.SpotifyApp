3cf86eb9e1873d89cbe7c05407ee3f2f003fc765
Francisco Martin Aguilar <fcomartinaguilar@gmail.com>
2018-05-18 17:55:40 +0200

    mi primer commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { APP_ROUTING } from './app.routes';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from "@angular/forms";
+
+//servicios
+import { SpotifyService} from './services/spotify.service'
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { SearchComponent } from './components/search/search.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { SinfotoPipe } from './pipes/sinfoto.pipe';
+import { DomseguroPipe } from './pipes/domseguro.pipe';
+
+import { ArtistComponent } from './components/artist/artist.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    SearchComponent,
+    NavbarComponent,
+    SinfotoPipe,
+    DomseguroPipe,
+    ArtistComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    APP_ROUTING
+  ],
+  providers: [SpotifyService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/services/spotify.service.ts
@@ -0,0 +1,66 @@
+import { Injectable, Pipe } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { map } from "rxjs/operators";
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SpotifyService {
+
+  artistas:any[] = [];
+  urlSpotify:string = 'https://api.spotify.com/v1/';
+  
+  //este token lo tengo q generar cada hora con el postman al ser una version gratuita (spotify api)
+  token:string = 'BQCVmi_Yxpz6Do06x9K173w_z7vnVupZcE9XO_h-8hDZfYR6TpWbPmi7R54VemIAS0AX7-LmAJ_uTphJddM';
+
+  constructor(public http:HttpClient) { }
+
+  private getHeaders():HttpHeaders {
+    let headers = new HttpHeaders({
+      'authorization':'Bearer ' + this.token
+    });
+    return headers;
+  }
+  
+  getTop(id:string) {
+    let url = `${this.urlSpotify}artists/${id}/top-tracks?country=ES`;
+    
+    let headers = this.getHeaders();
+
+    return this.http.get( url, {headers:headers} );
+  }
+
+  getArtista(id:string) {
+    let url = `${this.urlSpotify}artists/${id}`;
+
+    let headers = this.getHeaders();
+
+    // el subscribe es la respuesta -- no es necesario poner :headers
+   return this.http.get( url, {headers:headers} );
+        /* .pipe(map( (resp:any) => {
+            this.artistas = resp.artists.items;
+          return this.artistas;
+   })) */
+      /* .subscribe(resp => {
+        console.log(resp); 
+    }):*/
+  }
+
+  getArtistas(termino:string) {
+
+    let url = `${this.urlSpotify}search?query=${termino}&type=artist&limit=20`;
+
+    let headers = this.getHeaders();
+
+    // el subscribe es la respuesta
+   return this.http.get( url, {headers:headers} )
+        .pipe(map( (resp:any) => {
+            this.artistas = resp.artists.items;
+          return this.artistas;
+   }))
+      /* .subscribe(resp => {
+        console.log(resp); 
+    }):*/
+  }
+}
